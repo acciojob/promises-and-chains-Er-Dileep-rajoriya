74bd489674edd2463becbac495b926c77be91563
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-10-22 17:48:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let ageInput = document.getElementById("age");
+      let nameInput = document.getElementById("name");
+      let btn = document.getElementById("btn");
+
+      btn.addEventListener("click", () => {
+        let name = nameInput.value;
+        let age = ageInput.value;
+
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (age > 18) {
+              return resolve(`Welcome, ${name}. You can vote.`);
+            } else {
+              return reject(`Oh sorry ${name}. You aren't old enough.`);
+            }
+          }, 4000);
+        })
+          .then((data) => {
+            alert(data);
+          })
+          .catch((err) => {
+            alert(err);
+          });
+
+        // ageInput.value = "";
+        // nameInput.value = "";
+      });
